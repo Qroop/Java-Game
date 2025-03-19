77ca93e7fb8c76af8d9e7039361d55d0cec81a9b
jeswa278 <jeswa278@student.liu.se>
2025-03-19 14:39:56 +0100

    Testing

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    int maxScore = 500;
+    int userScore = 435;
+
+    float percentage = (float) userScore / maxScore * 100.0f;
+
+    System.out.println("User percentage is: " + percentage);
+  }
+}
