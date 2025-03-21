9ab5a7a0134b784646bcbdfebe8437031e754866
jeswa278 <jeswa278@student.liu.se>
2025-03-21 12:44:09 +0100

    Added gitignore and removed .class files

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
+import java.io.*;
+import javax.swing.*;
+
 public class Main {
   public static void main(String[] args) {
-    int maxScore = 500;
-    int userScore = 435;
-
-    float percentage = (float) userScore / maxScore * 100.0f;
-
-    System.out.println("User percentage is: " + percentage);
+    
   }
 }
